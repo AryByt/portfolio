fea5ca910abe4dd9b523caf9424788d4c25846da
arybyt <bayatiarya@gmail.com>
2021-08-04 08:40:19 -0500

    added more skills and languages

--- src/components/data/resume_data.js
+++ src/components/data/resume_data.js
@@ -1,55 +1,80 @@
-import react from '../../assets/icons/react.svg'
+import react from '../../assets/icons/react.svg';
 
 const languages = [
-    {
-        icon: react,
-        name: 'JavaScript',
-        level: '60'
-    },
-    {
-        icon: react,
-        name: 'HTML',
-        level: '80'
+  {
+    icon: react,
+    name: 'JavaScript',
+    level: '60',
   },
   {
     icon: react,
+    name: 'HTML',
+    level: '80',
+  },
+  {
+    icon: react,
+    name: 'React',
+    level: '90',
+  },
+  {
+    icon: react,
+    name: 'Express',
+    level: '70',
+  },
+  {
+    icon: react,
+    name: 'mongoDB',
+    level: '70',
+  },
+  {
+    icon: react,
+    name: 'Node',
+    level: '50',
+  },
+
+  {
+    icon: react,
     name: 'SQL',
-    level: '40'
-},
+    level: '40',
+  },
   {
     icon: react,
     name: 'CSS',
-    level: '80'
-},
-    {
-        icon: react,
-        name: 'postgreSQL',
-        level: '50'
-    },
-]
+    level: '80',
+  },
+  {
+    icon: react,
+    name: 'postgreSQL',
+    level: '50',
+  },
+];
 
 const tools = [
-    {
-        icon: react,
-        name: 'Figma',
-        level: '60'
-    },
-    {
-        icon: react,
-        name: 'Illustrator',
-        level: '40'
+  {
+    icon: react,
+    name: 'Figma',
+    level: '60',
+  },
+  {
+    icon: react,
+    name: 'Git & Gitflow',
+    level: '70',
+  },
+  {
+    icon: react,
+    name: 'Illustrator',
+    level: '40',
   },
   {
     icon: react,
     name: 'Bootstrap',
-    level: '60'
+    level: '60',
   },
   {
     icon: react,
     name: 'Tailwind',
-    level: '80'
-},
+    level: '80',
+  },
+];
 
-]
-
-export { tools, languages }
+export { tools, languages };
